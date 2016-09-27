99b87335bfebfa9cb8294fdb7e2c806f9944dac1
Revathi Uddaraju <revathiu@codeaurora.org>
2016-09-27 15:05:13 -0700

    audioflinger: Throttle output if no active tracks
    Throttle output for the duration from when there are
    no active tracks to the time output is put to
    standby due to inactivity. This is done to save some
    power during this duration. A typical usecase affected
    by this optimization is key press on the dialer
    keypad or keyboard with a delay of a few hundred
    milliseconds between keys.
    
    CRs-Fixed: 2088743
    Change-Id: I5b73c18a7b7767568e2461c72d8d7f86cafc9b7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -765,6 +765,7 @@ protected:
 
     // ThreadBase virtuals
     virtual     void        preExit();
+    virtual     void        onIdleMixer();
 
     virtual     bool        keepWakeLock() const { return true; }
     virtual     void        acquireWakeLock_l() {
@@ -1165,6 +1166,7 @@ protected:
                 bool        mHwSupportsPause;
                 bool        mHwPaused;
                 bool        mFlushPending;
+                bool        mHwSupportsSuspend;
                 // volumes last sent to audio HAL with stream->setVolume()
                 float mLeftVolFloat;
                 float mRightVolFloat;
@@ -1209,6 +1211,7 @@ protected:
     virtual     void        threadLoop_standby();
     virtual     void        threadLoop_mix();
     virtual     void        threadLoop_sleepTime();
+    virtual     void        onIdleMixer();
     virtual     uint32_t    correctLatency_l(uint32_t latency) const;
 
     virtual     status_t    createAudioPatch_l(const struct audio_patch *patch,
@@ -1232,6 +1235,7 @@ private:
                 // accessible only within the threadLoop(), no locks required
                 //          mFastMixer->sq()    // for mutating and pushing state
                 int32_t     mFastMixerFutex;    // for cold idle
+                int64_t     mIdleTimeOffsetUs;
 
                 std::atomic_bool mMasterMono;
 public:
