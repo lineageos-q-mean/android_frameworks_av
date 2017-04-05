24ed4a332b6f7c1ed214631a3b3860d9c2c9d5ad
Revathi Uddaraju <revathiu@codeaurora.org>
2017-04-05 15:09:14 +0530

    audio: add support to enable track offload using direct output
    This change squashes all changes done to enable track offload
    using direct output.
    
    AudioTrack: Set DIRECT track flag for direct pcm output
    libmedia : audio: handle set playback rate when direct pcm used
    audio: use offload standby delay for direct pcm
    audio: prohibit track reusing if effect chain moves
    audioflinger: Always process volume commands from a latest track
    audioflinger: pause direct output when buffer timeout
    audioflinger: remove redundant adding to tracksToRemove
    Change-Id: I2ad7eacf11642a4ca9f892b61124293d0dc503a9
    
    AudioTrack: Check original flags for track validation
    Change-Id: I7d3b831b8b49fd5fac8aee568cdf7685e3448e6b
    
    audio: follow-up change for track offload using direct output
    Change-Id: Ic72d373f9e7d5d420ccc1b4c0dca76794dbcc5e5
    
    nuplayer: handle setPlaybackSettings on direct tracks
    Change-Id: I41d096c4d5929e48577ac0bcb08835eabe127f07
    
    AudioFlinger: re-organize position adjustment for restarted DirectOutputs
    Change-Id: I8405e9f9a96ee8aef3281e8ed45506b60d5a5b44
    
    audioflinger: exclude idle frames for calculating position
    Change-Id: Ie35fe73d1540508132d3210657888f14e044df10
    
    audio: enable post processing effects on DIRECT output thread
    Change-Id: I7d9c154a7447cbfcf1f0066672ee81a5dbb0713c
    
    AudioTrack: Don't tear down sink for invalid offload track
    Change-Id: Ib39b5673ec4456d7b747a5ad108a1fc90f7f50de
    
    audiopolicy: fix unexpected output selection for direct pcm usecase
    Change-Id: Ic13773de4246c9dcc2f5966473efc395b59f9484
    `
    
    CRs-Fixed: 2199976
    Change-Id: Ibc2f7e6ecfa41b96ef94b6bb4e392fd888c5988a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -867,7 +867,7 @@ public:
 
     virtual     size_t      frameCount() const { return mNormalFrameCount; }
 
-                status_t    getTimestamp_l(AudioTimestamp& timestamp);
+    virtual     status_t    getTimestamp_l(AudioTimestamp& timestamp);
 
                 void        addPatchTrack(const sp<PatchTrack>& track);
                 void        deletePatchTrack(const sp<PatchTrack>& track);
@@ -1322,6 +1322,10 @@ protected:
     float                   mMasterBalanceLeft = 1.f;
     float                   mMasterBalanceRight = 1.f;
 
+    uint64_t                mFramesWrittenAtStandby;// used to reset frames on track reset
+    uint64_t                mFramesWrittenForSleep; // used to reset frames on track removal
+                                                    // or underrun before entering standby
+
 public:
     virtual     bool        hasFastMixer() const { return false; }
 
@@ -1343,6 +1347,7 @@ public:
                     }
                     return INVALID_OPERATION;
                 }
+    virtual     status_t    getTimestamp_l(AudioTimestamp& timestamp) override;
 };
 
 class OffloadThread : public DirectOutputThread {
